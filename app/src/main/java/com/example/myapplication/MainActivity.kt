18d630af0d32fd09c47ae7bc3fbc76e1953808da
Nouhaus <nouhaus@Nouhausui-MacBookPro.local>
2023-08-02 13:23:02 +0900

    testing4

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var test = "test"
+        
         var test2 = "test2"
     }
 }
